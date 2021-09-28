9d6d1d8fde3735328bff5583ae0deaa6b62404c3
Sijan Malla <44745915+season101@users.noreply.github.com>
2021-09-28 05:00:23 +0000

    Made changes to include input from user for desired integers a and b and calculate their gcd(a,b)

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # Euclidean Algorithm or popularly known as Divison with Remainder is an efficient way of finding gcd(a,b) of integers that are very long and inefficient to calculate by factorisation.
 
 def gcd(a,b):
+	print ("\nEuclidean Divison Algorithm\n")
 	while b!=0:
 		t = b
 		b = a % b
-		print (str(a) + " = " + str(t) +"." + str(a//t) + " + " + str(b)+"\n")
+		print (str(a) + " = " + str(t) +"." + str(a//t) + " + " + str(b))
 		a = t
 		
 	return a
 
-print("Greatest Common Divisor: "+ str(gcd(2024,748)))
+a = input("Enter first integer: ")
+b = input("Enter second integer: ")
+print("\nGreatest Common Divisor: "+ str(gcd(int(a),int(b))))
